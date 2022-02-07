3fcd7e81ec2ab242866ff93a79d25da7bebe0b04
aogu <aogu89goo415@gmail.com>
2022-02-07 20:13:18 +0900

    enable to filter and sort both and completed

--- src/components/CarECommerce.vue
+++ src/components/CarECommerce.vue
@@ -1,12 +1,12 @@
 <script setup >
 import ItemCard from './item-card.vue'
-import {computed, ref} from 'vue';
+import { computed, ref } from 'vue';
 
 const items = [
   {
     name: "Model 3",
     category: "Tesla",
-    price: "$48,490",
+    price: "48,490",
     date: "2020-09-04",
     imgUrl: "https://media.istockphoto.com/photos/electric-sports-car-the-tesla-model-3-picture-id1277254968?k=6&m=1277254968&s=612x612&w=0&h=4j3WhZQD95BJyqOnML2sDatX9SFQomJclLyd5bbV3FU="
   },
@@ -14,7 +14,7 @@ const items = [
   {
     name: "Model X",
     category: "Tesla",
-    price: "$89,990",
+    price: "89,990",
     date: "2017-08-08",
     imgUrl: "https://media.istockphoto.com/photos/tesla-model-x-travelling-on-the-freeway-in-silicon-valley-picture-id1191552504?k=6&m=1191552504&s=612x612&w=0&h=NWHap01jKkYzC5BT-9gaKYr8phGeO1ZUYHnGPLQ5RUM="
   },
@@ -22,7 +22,7 @@ const items = [
   {
     name: "Model S",
     category: "Tesla",
-    price: "$82,990",
+    price: "82,990",
     date: "2019-04-01",
     imgUrl: "https://media.istockphoto.com/photos/tesla-model-s-on-the-highway-picture-id582261244?k=6&m=582261244&s=612x612&w=0&h=LwYAD7oI6Xy9gsRM7OZ10Nsq-ehGsSmLcbPwQEmZBpg="
   },
@@ -30,7 +30,7 @@ const items = [
   {
     name: "Model Y",
     category: "Tesla",
-    price: "$48,190",
+    price: "48,190",
     date: "2020-02-02",
     imgUrl: "https://media.istockphoto.com/photos/tesla-model-y-in-austin-texas-at-360-bridge-picture-id1272025387?k=6&m=1272025387&s=612x612&w=0&h=o_W18f-pFNdHsSpdaNejXSS7G8XnWfayhinzZVWhQU8="
   },
@@ -38,7 +38,7 @@ const items = [
   {
     name: "Cayenne",
     category: "Porsche",
-    price: "$67,500",
+    price: "67,500",
     date: "2017-12-20",
     imgUrl: "https://media.istockphoto.com/photos/porsche-cayenne-picture-id1138819485?k=6&m=1138819485&s=612x612&w=0&h=uSPCZUofVb5ComBHUpH5qZ5QB7B1WWcKFxNzm53W2ME="
 
@@ -47,7 +47,7 @@ const items = [
   {
     name: "Macan",
     category: "Porsche",
-    price: "$52,100",
+    price: "52,100",
     date: "2019-07-12",
     imgUrl: "https://media.istockphoto.com/photos/porsche-macan-gts-crossover-suv-picture-id508107322?k=6&m=508107322&s=612x612&w=0&h=syLuqvZng-mEbqJblhY3q6wWxcUlaijknWO9ea90CJo="
 
@@ -56,7 +56,7 @@ const items = [
   {
     name: "Camry",
     category: "Toyota",
-    price: "$24,425",
+    price: "24,425",
     date: "2015-06-29",
     imgUrl: "https://media.istockphoto.com/photos/toyota-camry-in-motion-picture-id905266220?k=6&m=905266220&s=612x612&w=0&h=8iRQyhjJYVn_ppC6x3LTLGzG0P67NyCvuQqxvO40ryI="
 
@@ -65,7 +65,7 @@ const items = [
   {
     name: "Accord",
     category: "Honda",
-    price: "$24,800",
+    price: "24,800",
     date: "2018-10-02",
     imgUrl: "https://media.istockphoto.com/photos/honda-accord-in-the-city-picture-id649004692?k=6&m=649004692&s=612x612&w=0&h=7WnRBZcLQ6-Y2FrKKVxya3nwJ6U-CLeZUuk-D5RVlu8="
 
@@ -74,7 +74,7 @@ const items = [
   {
     name: "Civic",
     category: "Honda",
-    price: "$20,650",
+    price: "20,650",
     date: "2015-04-04",
     imgUrl: "https://media.istockphoto.com/photos/new-honda-civic-front-picture-id458119929?k=6&m=458119929&s=612x612&w=0&h=h8uh7Bqgw2l1Hhg2TmX05VSBnp2cxrjpTBtvPYpKiD4="
 
@@ -82,7 +82,7 @@ const items = [
   {
     name: "CX-5",
     category: "Mazda",
-    price: "$26,940",
+    price: "26,940",
     date: "2012-11-03",
     imgUrl: "https://media.istockphoto.com/photos/new-2018-mazda-cx5-red-cx5-suv-car-japanese-car-moving-vehicle-picture-id1069236296?k=6&m=1069236296&s=612x612&w=0&h=CO2YJA51esFD2an-sKkhusVifvwTwhBXR3ebxD6h3Fg="
 
@@ -90,7 +90,7 @@ const items = [
   {
     name: "GLE Coupe",
     category: "Mercedes-Benz",
-    price: "$76,500",
+    price: "76,500",
     date: "2020-02-05",
     imgUrl: "https://media.istockphoto.com/photos/mercedesbenz-gle-43-4matic-coupe-picture-id894961460?k=6&m=894961460&s=612x612&w=0&h=aKbMi31eXuJOSVEKlesIzsJlLM6QhUe0k_5V-FJvH0M="
 
@@ -98,7 +98,7 @@ const items = [
   {
     name: "CLA",
     category: "Mercedes-Benz",
-    price: "$37,850",
+    price: "37,850",
     date: "2019-06-09",
     imgUrl: "https://media.istockphoto.com/photos/mercedes-benz-cla-picture-id472122163?k=6&m=472122163&s=612x612&w=0&h=IIRhsoWfMXXy6b_aTMtSA2dgSTPK4pSD-_M5vh8OY6M="
 
@@ -106,7 +106,7 @@ const items = [
   {
     name: "GLA 250",
     category: "Mercedes-Benz",
-    price: "$37,280",
+    price: "37,280",
     date: "2017-05-02",
     imgUrl: "https://media.istockphoto.com/photos/mercedesbenz-gla-2020-test-drive-day-picture-id1297389811?k=6&m=1297389811&s=612x612&w=0&h=NOPBUppwL9aFKKoha3phbhpHUw6hYSdsW5NlFejp4RU="
 
@@ -114,7 +114,7 @@ const items = [
   {
     name: "RX 350",
     category: "Lexus",
-    price: "$45,175",
+    price: "45,175",
     date: "2015-01-01",
     imgUrl: "https://media.istockphoto.com/photos/hybrid-suv-lexus-nx-on-the-road-picture-id657915058?k=6&m=657915058&s=612x612&w=0&h=DGCgfv-pBE75oOUktrX_WpGSadnEmRcm5iPfa49BOQw="
 
@@ -122,7 +122,7 @@ const items = [
   {
     name: "NX 300",
     category: "Lexus",
-    price: "$37,510",
+    price: "37,510",
     date: "2018-09-12",
     imgUrl: "https://media.istockphoto.com/photos/lexus-nx-200t-car-picture-id480681192?k=6&m=480681192&s=612x612&w=0&h=Ne9_r4aioLH_jpiK41fHpTr-ADdKsHcFMOOPXT9DGp8="
 
@@ -130,7 +130,7 @@ const items = [
   {
     name: "Urus",
     category: "Lamborghini",
-    price: "$218,000",
+    price: "218,000",
     date: "2021-08-16",
     imgUrl: "https://media.istockphoto.com/photos/lamborghini-urus-picture-id1184360237?k=6&m=1184360237&s=612x612&w=0&h=cDC3bjJCG9fZC8VpJnuoK73du-hKLnij-14eKHxuLyA="
 
@@ -138,7 +138,7 @@ const items = [
   {
     name: "Aventador",
     category: "Lamborghini",
-    price: "$393,695",
+    price: "393,695",
     date: "2020-09-11",
     imgUrl: "https://media.istockphoto.com/photos/lamborghini-aventador-lp-7504-superveloce-picture-id1083962000?k=6&m=1083962000&s=612x612&w=0&h=VBYRfp0408ZHqDTdJ69pIJP7z_zhj2T7z8OB-28adSU="
 
@@ -146,7 +146,7 @@ const items = [
   {
     name: "A3",
     category: "Audi",
-    price: "$33,500",
+    price: "33,500",
     date: "2019-05-08",
     imgUrl: "https://media.istockphoto.com/photos/audi-a3-sportback-on-a-street-picture-id1255388367?k=6&m=1255388367&s=612x612&w=0&h=nh2TLu9W-68-3_PgN1-0qkXYaw4mfFZviKY1sY_fjJ8="
 
@@ -154,7 +154,7 @@ const items = [
   {
     name: "X3",
     category: "BMW",
-    price: "$43,000",
+    price: "43,000",
     date: "2018-03-11",
     imgUrl: "https://media.istockphoto.com/photos/-picture-id1206921084?k=6&m=1206921084&s=612x612&w=0&h=o8ETeAQHAuzOerMorNWxPnDpyhSyrxiy6vvIQ8TLd4Y="
 
@@ -162,29 +162,66 @@ const items = [
   {
     name: "2 Series",
     category: "BMW",
-    price: "$37,500",
+    price: "37,500",
     date: "2019-01-15",
     imgUrl: "https://media.istockphoto.com/photos/coupe-competition-picture-id1187329409?k=6&m=1187329409&s=612x612&w=0&h=qDHLX8yA8WVGmEwFU8k56z72uobZfcGkHH14zzpLPc4="
   },
 ];
+const defaultOrder =JSON.parse(JSON.stringify(items))
+let selected = ref("Category");
+let sorted = ref("Sort by");
+// const categoryFilteringAndSorting = computed(() => {
+//   if(sorted.value == 1) console.log("jlkfds");
+//   else if(sorted.value == 2) console.log("jlkfds")
+//   else if(sorted.value == 3) .sort((a, b) => new Date(a.created) - new Date(b.created));
 
-let selected = ref("");
 
-const categoryFiltering = computed(() => {
-  //console.log(selected.value);
-  if(selected.value == "Category") return items;
+//   if(selected.value == "Category") return items;
+//   else return items.filter((item) => item.category === selected.value);
+
+// } );
+
+const categoryFilteringAndSorting = computed(() => {
+  // let temp = filtering(items);
+  // return sorting(temp);
+  return sorting(filtering(items));
+
+});
+
+const filtering = (items) => {
+  if (selected.value == "Category") return items;
   else return items.filter((item) => item.category === selected.value);
-  // if(selected.value == "1") return items.filter((item) => item.category === "Tesla")
-  // else if(selected.value == "2") return items.filter((item) => item.category === "Porsche");
-  // else return items;
-} );
+}
+
+const sorting = (items) => {
+  if (sorted.value == "1") {
+      return items.sort(function(a, b) {
+      return parseFloat(a.price.replace(/,/g, '')) - parseFloat(b.price.replace(/,/g, ''));
+      });
+  }
+  else if (sorted.value == "2"){
+      return items.sort(function(a, b) {
+      return parseFloat(b.price.replace(/,/g, '')) - parseFloat(a.price.replace(/,/g, ''));
+      });
+  }
+  else if (sorted.value == "3") return items.sort((a, b) => new Date(b.date) - new Date(a.date))
+  else{
+    return defaultOrder;
+  }
+}
 
 
 
 
 
 
-// console.log(categoryFiltering);
+
+
+
+
+
+
+
 </script>
 <template>
   <div class="container mt-5">
@@ -206,8 +243,8 @@ const categoryFiltering = computed(() => {
       </div>
 
       <div>
-        <select class="form-select" aria-label="Default select example">
-          <option selected>Sort by:</option>
+        <select class="form-select" v-model="sorted" aria-label="Default select example">
+          <option selected value="Sort by">Sort by:</option>
           <option value="1">Price: Low to High</option>
           <option value="2">Price: High to Low</option>
           <option value="3">Newest Arrivals</option>
@@ -217,7 +254,7 @@ const categoryFiltering = computed(() => {
 
     <div class="d-flex flex-wrap pt-4">
       <ItemCard
-        v-for="item in categoryFiltering"
+        v-for="item in categoryFilteringAndSorting"
         :key="item.name"
         :item="{ name: item.name, category: item.category, price: item.price, date: item.date, imgUrl: item.imgUrl }"
       ></ItemCard>
--- src/components/item-card.vue
+++ src/components/item-card.vue
@@ -13,7 +13,7 @@ const props = defineProps({
       <img :src="props.item.imgUrl" class="card-img-top" alt="..." />
       <h5 class="card-title pt-2">{{ props.item.name }}</h5>
       <p class="card-text">Category: {{ props.item.category }}</p>
-      <p class="card-text">Price: {{ props.item.price }}</p>
+      <p class="card-text">Price: ${{ props.item.price }}</p>
       <p class="card-text">Date:{{ props.item.date }}</p>
     </div>
   </div>
